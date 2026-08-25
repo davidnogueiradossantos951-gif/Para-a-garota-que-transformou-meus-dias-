499bdec4e03efcaf369b9283a5d31bc598e0f591
davidnogueiradossantos951-gif <davidnogueiradossantos951@gmail.com>
2026-08-24 22:22:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function entrarNaHistoria() {
 
+    document.getElementById("historia").scrollIntoView({
+        behavior: "smooth"
+    });
+
+}
+
+function proximaParte() {
+
+    alert("Essa parte ainda vai ser escrita... ❤️");
+
+}
