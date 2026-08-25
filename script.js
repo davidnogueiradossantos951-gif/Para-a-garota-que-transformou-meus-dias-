156059e3b36c858896843d765e5d350c87fb61ca
davidnogueiradossantos951-gif <davidnogueiradossantos951@gmail.com>
2026-08-24 22:47:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ function proximaParte() {
     alert("Essa parte ainda vai ser escrita... ❤️");
 
 }
+function atualizarContador() {
+
+    const inicio = new Date(2026, 5, 10);
+    const hoje = new Date();
+
+    inicio.setHours(0, 0, 0, 0);
+    hoje.setHours(0, 0, 0, 0);
+
+    const diferenca = hoje - inicio;
+
+    const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+
+    document.getElementById("dias").textContent = dias;
+}
+
+atualizarContador();
